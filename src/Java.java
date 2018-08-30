1b48f470864db9e993e7d538e41a3208e821f843
Millenadv <millenasferraroni@gmail.com>
2018-08-30 19:20:38 -0300

    PrimeiraAulaJava

---
+++ src/Java.java
@@ -0,0 +1,19 @@
+
+public class Java {
+public static double div(int n1, int n2) {
+double total = n1 / n2;
+return total;
+
+}
+
+
+public static int sub(int n1, int n2) {
+	int total = n1 - n2;
+	return total;
+}
+
+public static void main(String[] args) {
+	System.out.println(div(40, 2));
+	System.out.println(sub(50, 20));
+}
+}
